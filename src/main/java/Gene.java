fd84f9bc0afadbe878d82753788578a61b1b6080
genehern <genehernlee@gmail.com>
2025-08-14 17:26:48 +0800

    Change chatbot name and corresponding text

---
+++ src/main/java/Gene.java
@@ -0,0 +1,11 @@
+public class Gene {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n" +
+                " Hello! I'm Gene\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
