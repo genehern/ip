e8e884a3a68af9ef82340d2af404aa1ac04bdae2
genehern <genehernlee@gmail.com>
2025-08-14 18:01:48 +0800

    Prints out user input and exit on bye

--- src/main/java/Gene.java
+++ src/main/java/Gene.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class Gene {
+    private static final String LINE = "____________________________________________________________\n";
+
+    private static final String SPACING = "    ";
+
+    private static void printFormatResponse(String s){
+        System.out.println(LINE);
+        System.out.println(SPACING + s);
+        System.out.println(LINE);
+    }
+
+    private static void printGreeting(){ System.out.println(LINE +
+            "Hello! I'm Gene\n" +
+            "What can I do for you?\n" +
+            LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                " Hello! I'm Gene\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(logo);
+        Scanner sc = new Scanner(System.in);
+
+       printGreeting();
+
+        while (true){
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+               printFormatResponse("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println("     " + input);
+            System.out.println(LINE);
+        }
     }
 }
